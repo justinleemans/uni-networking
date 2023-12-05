8d00a4c6a92acabf3510467e2dbffd45376db2be
Justin Leemans <justintentje64@gmail.com>
2023-12-05 19:41:54 +0100

    Fixed property accesibility on NetworkSignal class

--- Runtime/Domain/NetworkSignal.cs
+++ Runtime/Domain/NetworkSignal.cs
@@ -5,6 +5,6 @@ namespace JeeLee.Networking.Domain
     public abstract class NetworkSignal : Signal, INetworkSignal
     {
         public abstract int Id { get; }
-        public bool IsIncoming { get; }
+        public bool IsIncoming { get; set; }
     }
 }
