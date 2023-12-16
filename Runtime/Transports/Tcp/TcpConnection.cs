6bb2208499f4d76c03301dd49734a9f10c4fba7f
Justin Leemans <justintentje64@gmail.com>
2023-12-16 14:30:01 +0100

    Implemented first iteration of tcp implementation

--- Runtime/Transports/Tcp/TcpConnection.cs
+++ Runtime/Transports/Tcp/TcpConnection.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Net.Sockets;
 
@@ -6,6 +7,9 @@ namespace JeeLee.Networking.Transports.Tcp
     public class TcpConnection : Connection
     {
         private readonly Socket _socket;
+        private readonly byte[] _sizeBuffer = new byte[sizeof(int)];
+
+        private int _nextMessageSize;
 
         public TcpConnection(Socket socket, IPEndPoint endPoint) : base(endPoint.GetHashCode())
         {
@@ -14,12 +18,27 @@ namespace JeeLee.Networking.Transports.Tcp
 
         protected override void OnSend(byte[] dataBuffer)
         {
-            throw new System.NotImplementedException();
+            _socket.Send(dataBuffer, SocketFlags.None);
         }
 
         protected override void OnReceive(out byte[] dataBuffer)
         {
-            throw new System.NotImplementedException();
+            if (_nextMessageSize <= 0 && _socket.Available >= sizeof(int))
+            {
+                _socket.Receive(_sizeBuffer, sizeof(int), SocketFlags.None);
+                _nextMessageSize = BitConverter.ToInt32(_sizeBuffer);
+            }
+
+            if (_nextMessageSize > 0 && _socket.Available >= _nextMessageSize)
+            {
+                dataBuffer = new byte[_nextMessageSize];
+                _socket.Receive(dataBuffer, _nextMessageSize, SocketFlags.None);
+                _nextMessageSize = 0;
+            }
+            else
+            {
+                dataBuffer = null;
+            }
         }
 
         protected override void OnClose()
