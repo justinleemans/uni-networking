3dba92cf167031586ba38e5361a2a68b9764b8ef
Justin Leemans <justintentje64@gmail.com>
2024-05-14 20:02:50 +0200

    Added code comments to payload type enum

--- Runtime/Messages/Payloads/PayloadType.cs
+++ Runtime/Messages/Payloads/PayloadType.cs
@@ -1,5 +1,8 @@
 namespace JeeLee.UniNetworking.Messages.Payloads
 {
+    /// <summary>
+    /// Represents the type of payload used in network communications.
+    /// </summary>
     public enum PayloadType
     {
         Message,
